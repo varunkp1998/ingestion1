a836fac2044cc25cd33f348cb684cd228c888675
varunkp1998 <54853805+varunkp1998@users.noreply.github.com>
2022-06-23 22:33:37 +0530

    ingestion

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ echo “Building React Project …”
 npm run build 
 echo “Copying html file …” 
 cp -r build html 
-echo “Connecting to AWS VM and copying file to /var/www/html/ …” sudo scp -i ["C:/Users/varun/Music/ingestion.pem"] -r html [ec2-44-201-197-243.compute-1.amazonaws.com]:/var/www 
+echo “Connecting to AWS VM and copying file to /var/www/html/ …” sudo scp -i ["C:/Users/varun/Music/ingestion.pem"] -r html [ec2-user@ec2-44-201-197-243.compute-1.amazonaws.com]:/var/www 
 echo “Removing html file from local directory …” 
 rm -r html
